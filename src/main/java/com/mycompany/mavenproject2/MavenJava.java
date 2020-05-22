a87d3cdb3aad5b02c7fc098d9707ef808d09ba55
Gobind1507 <Gobind1507@yahoo.com>
2020-05-22 18:15:08 +0530

    adding via git2 folder2

---
+++ src/main/java/com/mycompany/mavenproject2/MavenJava.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author Govind
+ */
+public class MavenJava {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World.. This is my java class used to publish or run via Git and Jenkins");
+    }
+}
